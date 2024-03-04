d3ecc695c6dc52cd6d70ee4c300ce6da6355b7ab
jgcnsny <thegreathappyvirus@gmail.com>
2024-03-04 19:18:41 +0800

    Week2 Task

---
+++ week02/dom-basics.js
@@ -0,0 +1,19 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Description of image");
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>CSE 121b</h2>"
+const newPar = document.createElement("p")
+newPar.innerHTML = "<p> 'Welcome to Javascript' </p>"
+newSection.appendChild(newPar)
+document.body.appendChild(newSection)
